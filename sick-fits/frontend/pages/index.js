6a3af09714bb45bfb5ce3835be756b73fd4940aa
Alper Sunar <ASunar@Paylocity.com>
2018-10-30 06:43:54 -0500

    Initial commit

---
+++ sick-fits/frontend/pages/index.js
@@ -0,0 +1,11 @@
+//nextjs imports react for us.;
+import Link from 'next/link';
+
+
+const Home = props => (
+    <div>
+        <p>Hey123!</p>
+
+    </div>
+);
+export default Home;
---
+++ sick-fits/frontend/pages/sell.js
@@ -0,0 +1,9 @@
+//nextjs imports react for us.
+import Link from 'next/link';
+
+const Sell = props => (
+    <div>
+        <p>Sell!</p>
+    </div>
+);
+export default Sell;
