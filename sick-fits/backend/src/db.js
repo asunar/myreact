4d20c378a02fb6110a0cab987a517354150aa398
Alper Sunar <ASunar@Paylocity.com>
2018-11-04 05:58:15 -0600

    CreateItem, GetAllItems

--- sick-fits/backend/src/db.js
+++ sick-fits/backend/src/db.js
@@ -1,11 +1,11 @@
 // This files connects to the remote prisma DB and gives us the ability to query it with JS.
 
-const { Prisma } = require("prisma-binding"); //why are we using require on the serve...No import in node.js
+const { Prisma } = require("prisma-binding"); //why are we using require on the server...No import in node.js
 
 const db = new Prisma({
   typeDefs: "src/generated/prisma.graphql",
   endpoint: process.env.PRISMA_ENDPOINT,
-  secret: process.env.PRISMA_ENDPOINT,
+  secret: process.env.PRISMA_SECRET,
   debug: false //log queries in the console?
 });
 
