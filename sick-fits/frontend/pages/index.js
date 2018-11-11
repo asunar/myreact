9588d70f56ad4bacfb86d595479181d9f56677ce
Alper Sunar <ASunar@Paylocity.com>
2018-11-11 08:00:35 -0600

    Video 16 done

--- sick-fits/frontend/pages/index.js
+++ sick-fits/frontend/pages/index.js
@@ -1,11 +1,11 @@
-//nextjs imports react for us.;
-import Link from 'next/link';
+import Items from "../components/Items";
 
+//nextjs imports react for us.;
+//import Link from 'next/link';
 
 const Home = props => (
-    <div>
-        <p>Hey123!</p>
-
-    </div>
+  <div>
+    <Items />
+  </div>
 );
 export default Home;
